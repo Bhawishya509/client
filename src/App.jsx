dcfe51d0111ebedd68503d2573db8f852cb3c64c
Bhawishya singh rajput <lollysingh509@gmail.com>
2025-07-01 10:39:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+const App = () => {
+  const [data, setData] = React.useState("")
+  const clicked= async()=>
+  {
+   const response=prompt("Enter your name");
+  let res=await axios.get(`http://localhost:9000/${response}`)
+  setData(res.data)
+  console.log(res.data)
+  }
+  return (
+   <>
+    <div style={{height:"100vh",width:"100vw",backgroundColor:data}}>
+      hh
+    </div>
+
+    <button onClick={clicked}>click</button>
+   </>
+  )
+}
+
+export default App
